1a2b9126320f557d72373e9f8e8bc28908513305
Igor Andruskiewitsch <i.andruskiewitsch23@gmail.com>
2024-03-11 16:55:29 -0300

    Add initial app and coordinator files

---
+++ {{ cookiecutter.project_slug }}/Shell/Sources/AppCoordinator.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+/// The coordinator for the whole application.
+struct AppCoordinator: View {
+
+    // MARK: Body
+
+    var body: some View {
+        Text("{{ cookiecutter.project_name }}")
+    }
+}
+
---
+++ {{ cookiecutter.project_slug }}/Shell/Sources/ShellApp.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+@main
+struct {{ cookiecutter.project_slug }}App: App {
+    var body: some Scene {
+        WindowGroup {
+            AppCoordinator()
+        }
+    }
+}
+
